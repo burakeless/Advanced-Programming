11bc2a5e8fafeacc51109de6ec025501e8873316
Burak Keles <kelesburak13@outlook.com>
2018-11-21 15:53:53 +0300

    vol3

--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,12 @@ self.addEventListener('install', installCB)
 
 //------------------------------------------------------------
 
-function save(req, resp) {
-    return caches.open(CACHE)
-        .then(cache => {
-            cache.put(req, resp.clone());
-            return resp;
-        })
-        .catch(console.log)
-}
-function fetchCB(e) { //fetch first
+function cacheCB(e) { //cache first
     let req = e.request
     e.respondWith(
-        fetch(req).then(r2 => save(req, r2))
-            .catch(() => { return caches.match(req).then(r1 => r1) })
+        caches.match(req)
+            .then(r1 => r1 || fetch(req))
+            .catch(console.log)
     )
 }
-self.addEventListener('fetch', fetchCB)
+self.addEventListener('fetch', cacheCB)
