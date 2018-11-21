6efff598ea080f4e9b7cb54cc96c20e3d1f3ba2f
Burak Keles <kelesburak13@outlook.com>
2018-11-21 20:45:14 +0300

    service worker

--- classWork7/sw.js
+++ classWork7/sw.js
@@ -1,25 +1,29 @@
-navigator.serviceWorker.register('https://github.com/burakeless/Advanced-Programming/blob/master/classWork7/sw.js')
+navigator.serviceWorker.register('https://burakeless.github.io/Advanced-Programming/classWork7/sw.js')
 
 const CACHE ='JS'
+const FILES = ['https://burakeless.github.io/Advanced-Programming/classWork7/cw7.html']
 function installCB(e) {
-  console.log('installed', e.request);
+    e.waitUntil(
+        caches.open(CACHE)
+            .then(cache => cache.addAll(FILES))
+            .catch(console.log)
+    )
 }
 self.addEventListener('install', installCB)
 
 function save(req, resp) {
-  return caches.open(CACHE)
-  .then(cache => {
-    cache.put(req, resp.clone());
-    return resp;
-  }) 
-  .catch(console.log)
+    return caches.open(CACHE)
+        .then(cache => {
+            cache.put(req, resp.clone());
+            return resp;
+        })
+        .catch(console.log)
 }
 function fetchCB(e) { //fetch first
-  let req = e.request
-  console.log('JS', req.url);
-  e.respondWith(
-    fetch(req).then(r2 => save(req, r2))
-    .catch(() => { return caches.match(req).then(r1 => r1) })
-  )
+    let req = e.request
+    e.respondWith(
+        fetch(req).then(r2 => save(req, r2))
+            .catch(() => { return caches.match(req).then(r1 => r1) })
+    )
 }
 self.addEventListener('fetch', fetchCB)
