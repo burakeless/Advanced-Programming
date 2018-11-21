487f8dad35f9be964bf82fe0a3bfd5b43c3fc3af
Burak Keles <kelesburak13@outlook.com>
2018-11-21 16:12:03 +0300

    auto cache

--- service-worker.js
+++ service-worker.js
@@ -11,12 +11,19 @@ self.addEventListener('install', installCB)
 
 //------------------------------------------------------------
 
-function cacheCB(e) { //cache first
+function save(req, resp) {
+    return caches.open(CACHE)
+        .then(cache => {
+            cache.put(req, resp.clone());
+            return resp;
+        })
+        .catch(console.log)
+}
+function fetchCB(e) { //fetch first
     let req = e.request
     e.respondWith(
-        caches.match(req)
-            .then(r1 => r1 || fetch(req))
-            .catch(console.log)
+        fetch(req).then(r2 => save(req, r2))
+            .catch(() => { return caches.match(req).then(r1 => r1) })
     )
 }
-self.addEventListener('fetch', cacheCB)
+self.addEventListener('fetch', fetchCB)
